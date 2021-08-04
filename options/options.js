3b36ffba7e8482ee094e92f769bbcbfa66063c9b
Crowdbotics <team@crowdbotics.com>
2021-08-04 13:46:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cloudiby_29449Identifier",
+  url: "https://cloudiby_29449Identifier.botics.co",
+  api: "https://cloudiby_29449Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
